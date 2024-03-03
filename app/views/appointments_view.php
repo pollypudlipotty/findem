88ae0a6c63783946a52a45e9dfa57a7995a00701
maaszMartina <maaszmartina@gmail.com>
2024-03-03 18:23:29 +0100

    profil

---
+++ app/views/appointments_view.php
@@ -0,0 +1,39 @@
+<div class="container">
+      <div class="row gy-3">
+        <div class="col-md-4">
+          <div class="card m-5">
+            <div class="card-body">
+              <h5 class="card-title">Szolgáltatás megnevezése</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>időpont: </span>adatbázisból</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>helyszín: </span>adatbázisból</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>megjegyzés: </span>Megjegyzés</h6>
+              <a href="#" class="btn btnReserve">Foglalás</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card m-5">
+            <div class="card-body">
+              <h5 class="card-title">Fodrászat</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>időpont: </span>2023-05-16 10:30-12:00</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>helyszín: </span>1065 Budapest, Révay utca 16.
+              </h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>megjegyzés: </span>Csak készpénzes fizetés!</h6>
+              <a href="#" class="btn btnReserve">Foglalás</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card m-5">
+            <div class="card-body">
+              <h5 class="card-title">Masszázs</h5>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>időpont: </span>2023-05-16 17:00-19:00</h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>helyszín: </span>1065 Budapest, Révay utca 16.
+              </h6>
+              <h6 class="card-subtitle mb-2 text-body-secondary"><span>megjegyzés: </span></h6>
+              <a href="#" class="btn btnReserve">Foglalás</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
