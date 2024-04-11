dba5bcd76646487ec7a62879b18c7d0d15050b20
kittitz <kittitozer@gmail.com>
2024-04-12 00:05:19 +0200

    icon changed

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FindEM</title>
-    <link rel="icon" href="/public/images/findEM_logo.png" type="image/x-icon" />
+    <link rel="icon" href="/public/images/findem_icon.png" type="image/x-icon" />
     <!-- BOOTSTRAP LINKEK-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
